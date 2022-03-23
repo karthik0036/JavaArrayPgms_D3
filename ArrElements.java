68dc6e180eaf0af17c3aa8b0e8e1ee8172a36b77
Karthik <jeefkarthik036@gmail.com>
2022-03-23 11:39:15 +0530

    second- UC-1 ArrayPgm

---
+++ ArrElements.java
@@ -0,0 +1,15 @@
+package com.bridge.D3_assignments;
+
+public class ArrElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {1,2,3,4,5};
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+
+	}
+
+}
