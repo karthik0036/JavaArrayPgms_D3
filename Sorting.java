4a4e28e827a6b4c68c1708d3687dab1e5cf099ca
Karthik <jeefkarthik036@gmail.com>
2022-03-23 14:16:26 +0530

    ninth-added UC-8 Pgm

---
+++ Sorting.java
@@ -0,0 +1,30 @@
+package com.bridge.D3_assignments;
+
+public class Sorting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {6,4,3,7,2,1,5};
+		
+		int temp=0;
+		for (int i = 0; i < arr.length; i++) {     
+            for (int j = i+1; j < arr.length; j++) {     
+               if(arr[i] > arr[j]) {    
+                   temp = arr[i];    
+                   arr[i] = arr[j];    
+                   arr[j] = temp;    
+               }     
+            }     
+        }    
+          
+        System.out.println();
+        //Displaying elements of array after sorting    
+        System.out.println("Elements of array sorted in ascending order: ");    
+        for (int i = 0; i < arr.length; i++) {     
+            System.out.print(arr[i] + " ");    
+        }    
+    }    
+}    
+        
+
+	
