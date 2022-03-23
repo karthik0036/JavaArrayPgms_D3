8d21c6eb33f66846284a13c77b0f7c09e3eb9715
Karthik <jeefkarthik036@gmail.com>
2022-03-23 13:06:38 +0530

    fourth- added UC-3 Pgm

---
+++ LargestElement.java
@@ -0,0 +1,19 @@
+package com.bridge.D3_assignments;
+
+public class LargestElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double[] myList = {1.2,2.3,3.4,4.5};
+		
+		//finding the max value
+		double max = 0;
+		for(int i=0;i<myList.length;i++) {
+			if( myList[i]>max)
+				max = myList[i];
+			}
+		System.out.println("max val is: "+max);
+
+	}
+
+}
